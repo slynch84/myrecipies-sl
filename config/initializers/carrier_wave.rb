051b9bdf155de22906d2b5464988954928295850
homelessone <scottlynchfilm@gmail.com>
2016-09-06 19:04:41 +0000

    again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,8 @@ if Rails.env.prodution?
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => 'us-west-1'
+      :endpoint => "https://s3.amazonaws.com",
+      :region => ENV['S3_REGION']
     }
     
     config.fog_directory = ENV['S3_BUCKET']  
